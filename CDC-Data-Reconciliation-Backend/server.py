0427e9f8ca15d6b8d5157b84e2573f971e7ad265
Mcharo, Solomon Kaluma <solomonmcharo28@gmail.com>
2023-11-24 16:41:43 -0500

    Did the compare and sends new data

--- CDC-Data-Reconciliation-Backend/server.py
+++ CDC-Data-Reconciliation-Backend/server.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, File, UploadFile
+import asyncio
+import csv
 #import pandas as pd
 app = FastAPI()
 import os
@@ -20,5 +22,20 @@ async def read_file(state_file: UploadFile = File(...), cdc_file:  UploadFile = 
   with open(state_save_to, "wb") as f:
     f.write(state_content)
   print(state_file.filename)
+  process = await asyncio.create_subprocess_exec(
+        'python', 'compare.py', '-c', cdc_save_to, '-s', state_save_to, '-o', f'./{folder_name}/results.csv'
+    )
+
+  await process.wait()
+  res_file = "./temp/results.csv"
+  res = []
+  with open(res_file, newline='') as csvfile:
+        # Create a CSV reader object
+        reader = csv.DictReader(csvfile)
+        # Loop through each row in the CSV file
+        for row in reader:
+            # Add the row as a dictionary to the list
+            res.append(row)
+  print(res)
   #return {"filename": filename, "content": content}
-  return f"We have read the file! Content: {cdc_content}"
+  return res
